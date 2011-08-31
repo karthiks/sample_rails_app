ae066ae1c8d08c52c49ab4aadfe1f77aa4f49530
Karthik Sirasanagandla <karthiks@thoughtworks.com>
2011-08-31 09:00:06 +0530

    Initial commit - Bare app without testing placeholder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8433aa6b17d2bb7231ecdbfcfaead9982a3cf0fd3e3c50571aad86bc070c3d017e74a9487418f1a67efc86433a8cec6597fbd10f84a607e6634c66f7308ad323'
